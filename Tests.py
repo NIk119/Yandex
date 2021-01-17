fa5769ad3b3314acd49cb32df377a7ba705ef271
NIk119 <77139254+NIk119@users.noreply.github.com>
2021-01-17 13:28:20 +0300

    Создали тест сьют.Пока все работает!

--- Tests.py
+++ Tests.py
@@ -6,39 +6,40 @@ from Dat import YandexSeacrhData as dat
 # @allure.feature('Open pages')
 # @allure.story('Открывает станицу')
 # @allure.severity('blocker')
-@allure.feature('Random Yandex')
-@allure.story('Получаем страницу почты')
-@allure.severity('blocker')
-def test_yandex_search(app):
-    #запускаем сайт
-    app.go_to_site()
+class TestYand:
+    @allure.feature('Random Yandex')
+    @allure.story('Получаем страницу почты')
+    @allure.severity('blocker')
+    def test_yandex_search(self,app):
+        #запускаем сайт
+        app.go_to_site()
 
-@allure.story('Заходим')
-@allure.severity('blocker')
-def test_yandex_in(app):
-    app.Yandex.enter_mail()
-    #входим на почту
-@allure.story('Логин')
-@allure.severity('critical')
-def test_yandex_login(app):
-    app.Yandex.enter_login(dat.LOGIN)
-    #вводим логин Входим (кнопку нажимаем)
-@allure.story('Пароль')
-@allure.severity('critical')
-def test_yandex_password(app):
-    app.Yandex.enter_password(dat.PASSWORD)
-    #вводим пароль входим в аккаунт
-@allure.story('пошел по почте')
-@allure.severity('normal')
-def test_yandex_mail(app):
-    app.Yandex.go_mail()
-    # пошли по почте
-@allure.story('отправка почты')
-@allure.severity('critical')
-def test_yandex_output(app):
-    app.Yandex.mail_post(dat.MAIL,dat.LAST_NAME)
-    # отправили почту
-    
+    @allure.story('Заходим')
+    @allure.severity('blocker')
+    def test_yandex_in(self,app):
+        app.Yandex.enter_mail()
+        #входим на почту
+    @allure.story('Логин')
+    @allure.severity('critical')
+    def test_yandex_login(self,app):
+        app.Yandex.enter_login(dat.LOGIN)
+        #вводим логин Входим (кнопку нажимаем)
+    @allure.story('Пароль')
+    @allure.severity('critical')
+    def test_yandex_password(self,app):
+        app.Yandex.enter_password(dat.PASSWORD)
+        #вводим пароль входим в аккаунт
+    @allure.story('пошел по почте')
+    @allure.severity('normal')
+    def test_yandex_mail(self,app):
+        app.Yandex.go_mail()
+        # пошли по почте
+    @allure.story('отправка почты')
+    @allure.severity('critical')
+    def test_yandex_output(self,app):
+        app.Yandex.mail_post(dat.MAIL,dat.LAST_NAME)
+        # отправили почту
+
     
 
 
